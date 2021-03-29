28ed23b296b7616f24ac8dd0f759208390ebed98
Bernard Tinashe <bentinkuyayama@gmail.com>
2021-03-29 23:45:26 +0200

    First push to gitHub

---
+++ ToDo.js
@@ -0,0 +1,16 @@
+var name=Bernard T Kuyayama;
+var input=prompt("What do you want to to?");
+while(input !=="quit"){
+	//handle input
+	if(input==="list"){
+	console.log(todos);
+}else if (input==="new") {
+	//ask for new todo
+	var newTodo=prompt("Enter new todo");
+	//add to todos array
+	todos.push(newTodo);
+}
+//ask again for new input
+input=prompt("What would you like to do?");
+}
+console.log("OK,YOU QUIT THE APP ");
